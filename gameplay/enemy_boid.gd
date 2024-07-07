ec31fb6b27f1b259d3c9e6e5d63f4f28e0b2987e
Dale <dalerhendricks@outlook.com>
2024-07-06 18:52:08 -0700

    Started working on the boid test field and EnemyBoid functionality

--- gameplay/enemy_boid.gd
+++ gameplay/enemy_boid.gd
@@ -1,2 +1,22 @@
 extends Area2D
 
+var vel := Vector2.ZERO
+var speed := 1.0
+
+func _ready():
+	randomize()
+	var size = get_viewport_rect().size
+	global_position = Vector2(
+		randf_range(0, size.x),
+		randf_range(0, size.y)
+	)
+	var startRotation = randf_range(0, 2 * PI)
+	global_rotation = startRotation
+	vel = Vector2.from_angle(rotation)
+
+func _process(_delta):
+	vel = vel.normalized() * speed
+	move()
+
+func move():
+	global_position += vel
