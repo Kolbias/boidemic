29f11d509c4cd5c727174f55ce707d3e88484465
Kolby <kolbyheiss@gmail.com>
2024-07-08 23:11:18 -0700

    Added player scene with animation and simple movement, added various additional art files

---
+++ gameplay/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+# Placeholder Mouse Movement
+func _process(delta):
+	look_at(get_global_mouse_position())
+
+func _physics_process(delta):
+	var vel : Vector2 = get_global_mouse_position() - global_position
+	velocity = vel
+	move_and_slide()
