b3165baa99426c2b22ce48c1253e0fd55a3cf7d3
Kolby <kolbyheiss@gmail.com>
2024-07-20 02:28:10 -0700

    Adjusted main menu

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,8 +1,8 @@
 extends Control
 
-
+func _process(delta):
+	$MainBoid.position += Vector2(1,-1)
+	if $MainBoid.position.y < -400:
+		$MainBoid.position = Vector2(-56, 760)
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
-
-func _on_quit_button_pressed():
-	get_tree().quit()
